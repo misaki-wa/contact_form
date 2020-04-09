c57c097d6d1cd9ad57f3e30adb04cc02af6696a6
misaki-wa <wakimisa@gmail.com>
2020-04-09 11:43:36 +0900

    adminlogin.php追加

---
+++ adminlogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset=utf-8">
+<title>管理者ログイン</title>
+</head>
+<body>
+<h1>管理者ログイン</h1>
+<form action="admin.php" method="POST">
+メールアドレス<input type="email" name="email">
+パスワード<input type="password" name="password">
+<input type="submit" value="ログイン">
+</form>
+</body>
+</html>
+
